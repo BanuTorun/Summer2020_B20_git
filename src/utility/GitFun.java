4c079e7a818828af89dd95625edfb67295612bcb
BanuTorun <banu.torun@hotmail.com>
2020-08-05 19:55:38 -0500

    add GitFun.java

---
+++ src/utility/GitFun.java
@@ -0,0 +1,11 @@
+package utility;
+
+public class GitFun {
+    public static void main(String[] args) {
+        int[]arr={1,2,3,4,5,6,7,8,9};
+
+        for(int i=0; i<10;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
