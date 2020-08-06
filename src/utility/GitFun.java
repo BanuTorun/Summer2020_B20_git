69d5ea934ec1525fc0fdc12ac09a609fe9388ee4
BanuTorun <68971649+BanuTorun@users.noreply.github.com>
2020-08-05 20:40:25 -0500

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -4,7 +4,7 @@ public class GitFun {
     public static void main(String[] args) {
         int[]arr={1,2,3,4,5,6,7,8,9};
         
-         System.out.println(arr);
+        
 
         for(int i=0; i<10;i++){
             System.out.println(arr[i]);
